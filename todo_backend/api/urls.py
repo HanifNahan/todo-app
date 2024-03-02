23354d2f18c189ed16d6ee38a9e56e2aee3daf49
HanifNahan <muhammad.hanif.mh639@gmail.com>
2024-03-02 22:13:38 +0800

    add backend

---
+++ todo_backend/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.getRoutes),
+  path('todos/', views.getTodos),
+  path('todos/create/', views.createTodo),
+  path('todos/<str:pk>/update/', views.updateTodo),
+  path('todos/<str:pk>/delete/', views.deleteTodo),
+  path('todos/<str:pk>/', views.getTodo),
+]
---
+++ todo_backend/api/views.py
@@ -0,0 +1,76 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TodoSerializer
+from .models import Todo
+
+@api_view(["GET"])
+def getRoutes(request):
+  routes = [
+    {
+      'Endpoint': '/todos/',
+      'method': 'GET',
+      'body': 'None',
+      'description': 'Returns an array of notes'
+    },
+    {
+      'Endpoint': '/todos/id',
+      'method': 'GET',
+      'body': 'None',
+      'description': 'Returns single todo object'
+    },
+    {
+      'Endpoint': '/todos/create/',
+      'method': 'POST',
+      'body': {'body': ""},
+      'description': 'Create new todo'
+    },
+    {
+      'Endpoint': '/todos/id/update/',
+      'method': 'PUT',
+      'body': {'body': ""},
+      'description': 'Update todo'
+    },
+    {
+      'Endpoint': '/todos/id/delete/',
+      'method': 'DELETEU',
+      'body': 'None',
+      'description': 'Delete todo'
+    },
+  ]
+  return Response(routes)
+
+@api_view(['GET'])
+def getTodos(request):
+  todo = Todo.objects.all()
+  serializer = TodoSerializer(todo, many=True)
+  return Response(serializer.data)
+
+@api_view(['GET'])
+def getTodo(request, pk):
+  todo = Todo.objects.get(id=pk)
+  serializer = TodoSerializer(todo, many=False)
+  return Response(serializer.data)
+
+@api_view(['POST'])
+def createTodo(request):
+  data = request.data
+  todo = Todo.objects.create(
+    body = data['body']
+  )
+  serializer = TodoSerializer(todo, many=False)
+  return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateTodo(request, pk):
+  data = request.data
+  todo = Todo.objects.get(id=pk)
+  serializer = TodoSerializer(todo, data=data)
+  if serializer.is_valid():
+    serializer.save()
+  return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteTodo(request, pk):
+  todo = Todo.objects.get(id=pk)
+  todo.delete()
+  return Response({"message": "Deleted successfully!"})
